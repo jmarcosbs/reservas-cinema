33bf6660a1f3c0218c5e0ffd240505c1bf5a8157
jmarcobs <jmarcosmmn@gmail.com>
2024-06-27 00:09:51 -0300

    tentando corrigir bugs em relação aos assentos

--- ReservasCinema/src/view/Main.java
+++ ReservasCinema/src/view/Main.java
@@ -30,6 +30,8 @@ import javax.swing.JToggleButton;
 import java.awt.Font;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class Main {
 
@@ -42,8 +44,9 @@ public class Main {
 	Usuario usuarioLogado;
 	List<Filme> listaDeFilmes = filmeDao.listarFilmes();
 	private final ButtonGroup buttonGroup = new ButtonGroup();
-	Filme filmeSelecionado;
+	private static Filme filmeSelecionado = null;
 	List<Assento> listaAssentos;
+	private JPanel panelEntrar;
 	
 
 	/**
@@ -85,7 +88,7 @@ public class Main {
 		frame.getContentPane().add(panel);
 		panel.setLayout(new CardLayout(0, 0));
 		
-		JPanel panelEntrar = new JPanel();
+		panelEntrar = new JPanel();
 		panel.add(panelEntrar, "painelEntrar");
 		panelEntrar.setLayout(null);
 		
@@ -127,6 +130,8 @@ public class Main {
 		});
 		btnLogin.setBounds(180, 235, 149, 23);
 		panelEntrar.add(btnLogin);
+		
+		
 		
 		txtEmail = new JTextField();
 		txtEmail.setBounds(180, 126, 149, 20);
@@ -216,6 +221,7 @@ public class Main {
 			public void actionPerformed(ActionEvent e) {
 				if (rdbtnFilme1.isSelected()) {
 					btnFilmeAvancar.setEnabled(true);
+					filmeSelecionado = listaDeFilmes.get(0);
 				}
 			}
 			
@@ -225,15 +231,14 @@ public class Main {
 		
 		rdbtnFilme1.setBounds(20, 270, 109, 23);
 		panelFilmes.add(rdbtnFilme1);
-		
-		System.out.println(listaDeFilmes.toString());
+	
 		
 		JRadioButton rdbtnFilme2 = new JRadioButton(listaDeFilmes.get(1).getTitulo());
 		rdbtnFilme2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (rdbtnFilme2.isSelected()) {
 					btnFilmeAvancar.setEnabled(true);
-					
+					filmeSelecionado = listaDeFilmes.get(1);
 				} 
 			}
 		});
@@ -246,6 +251,8 @@ public class Main {
 			public void actionPerformed(ActionEvent e) {
 				if (rdbtnFilme3.isSelected()) {
 					btnFilmeAvancar.setEnabled(true);
+					filmeSelecionado = listaDeFilmes.get(2);
+
 				} 
 			}
 		});
@@ -259,6 +266,7 @@ public class Main {
 		
 				if (rdbtnFilme4.isSelected()) {
 					btnFilmeAvancar.setEnabled(true);
+					filmeSelecionado = listaDeFilmes.get(3);
 				} 
 				
 			}
@@ -273,16 +281,12 @@ public class Main {
 		panelFilmes.add(lblPreço);
 		
 		JPanel panelAssentos = new JPanel();
-		panelAssentos.addComponentListener(new ComponentAdapter() {
+		panelAssentos.addFocusListener(new FocusAdapter() {
 			@Override
-			public void componentShown(ComponentEvent e) {
+			public void focusGained(FocusEvent e) {
 				
-
-				filmeSelecionado = getFilmeSelecionado(rdbtnFilme1, rdbtnFilme2, rdbtnFilme3, rdbtnFilme4);
 				
-				System.out.println(filmeSelecionado.toString());
-
-			
+				
 			}
 		});
 		panel.add(panelAssentos, "painelAssentos");
@@ -292,7 +296,6 @@ public class Main {
 		lblAssentos.setBounds(43, 33, 46, 14);
 		panelAssentos.add(lblAssentos);
 		
-		listaAssentos = assentoDao.listaAssentos(filmeSelecionado);
 		
 		JButton btnFilmeVoltar_1 = new JButton("Voltar");
 		btnFilmeVoltar_1.addActionListener(new ActionListener() {
@@ -318,85 +321,105 @@ public class Main {
 		btnFilmeAvancar_1.setBounds(421, 368, 89, 23);
 		panelAssentos.add(btnFilmeAvancar_1);
 		
-		JToggleButton tglbtnA1 = new JToggleButton(listaAssentos.get(0).getCodigoAssento());
-		tglbtnA1.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		tglbtnA1.setBounds(131, 80, 45, 45);
-		panelAssentos.add(tglbtnA1);
 		
-		JToggleButton tglbtnA2 = new JToggleButton("A2");
-		tglbtnA2.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		tglbtnA2.setBounds(186, 80, 45, 45);
-		panelAssentos.add(tglbtnA2);
 		
-		JToggleButton tglbtnA3 = new JToggleButton("A3");
-		tglbtnA3.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		tglbtnA3.setBounds(241, 80, 45, 45);
-		panelAssentos.add(tglbtnA3);
+		panelAssentos.addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentShown(ComponentEvent e) {
+				
+				if (filmeSelecionado != null) {
+				listaAssentos = assentoDao.listaAssentos(filmeSelecionado);
+				System.out.println(filmeSelecionado.getTitulo());
+				
+				JToggleButton tglbtnA1 = new JToggleButton(listaAssentos.get(0).getCodigoAssento());
+				tglbtnA1.setFont(new Font("Tahoma", Font.PLAIN, 8));
+				tglbtnA1.setBounds(131, 80, 45, 45);
+				panelAssentos.add(tglbtnA1);
+				
+				JToggleButton tglbtnA2 = new JToggleButton("A2");
+				tglbtnA2.setFont(new Font("Tahoma", Font.PLAIN, 8));
+				tglbtnA2.setBounds(186, 80, 45, 45);
+				panelAssentos.add(tglbtnA2);
+				
+				JToggleButton tglbtnA3 = new JToggleButton("A3");
+				tglbtnA3.setFont(new Font("Tahoma", Font.PLAIN, 8));
+				tglbtnA3.setBounds(241, 80, 45, 45);
+				panelAssentos.add(tglbtnA3);
+				
+				JToggleButton tglbtnA4 = new JToggleButton("A4");
+				tglbtnA4.setFont(new Font("Tahoma", Font.PLAIN, 8));
+				tglbtnA4.setBounds(296, 80, 45, 45);
+				panelAssentos.add(tglbtnA4);
+				
+				JToggleButton tglbtnB1 = new JToggleButton("B1");
+				tglbtnB1.setFont(new Font("Tahoma", Font.PLAIN, 8));
+				tglbtnB1.setBounds(131, 136, 45, 45);
+				panelAssentos.add(tglbtnB1);
+				
+				JToggleButton tglbtnB2 = new JToggleButton("B2");
+				tglbtnB2.setFont(new Font("Tahoma", Font.PLAIN, 8));
+				tglbtnB2.setBounds(186, 136, 45, 45);
+				panelAssentos.add(tglbtnB2);
+				
+				JToggleButton tglbtnB3 = new JToggleButton("B3");
+				tglbtnB3.setFont(new Font("Tahoma", Font.PLAIN, 8));
+				tglbtnB3.setBounds(241, 136, 45, 45);
+				panelAssentos.add(tglbtnB3);
+				
+				JToggleButton tglbtnB4 = new JToggleButton("B4");
+				tglbtnB4.setFont(new Font("Tahoma", Font.PLAIN, 8));
+				tglbtnB4.setBounds(296, 136, 45, 45);
+				panelAssentos.add(tglbtnB4);
+				
+				JToggleButton tglbtnC1 = new JToggleButton("C1");
+				tglbtnC1.setFont(new Font("Tahoma", Font.PLAIN, 8));
+				tglbtnC1.setBounds(131, 192, 45, 45);
+				panelAssentos.add(tglbtnC1);
+				
+				JToggleButton tglbtnC2 = new JToggleButton("C2");
+				tglbtnC2.setFont(new Font("Tahoma", Font.PLAIN, 8));
+				tglbtnC2.setBounds(186, 192, 45, 45);
+				panelAssentos.add(tglbtnC2);
+				
+				JToggleButton tglbtnC3 = new JToggleButton("C3");
+				tglbtnC3.setFont(new Font("Tahoma", Font.PLAIN, 8));
+				tglbtnC3.setBounds(241, 192, 45, 45);
+				panelAssentos.add(tglbtnC3);
+				
+				JToggleButton tglbtnC4 = new JToggleButton("C4");
+				tglbtnC4.setFont(new Font("Tahoma", Font.PLAIN, 8));
+				tglbtnC4.setBounds(296, 192, 45, 45);
+				panelAssentos.add(tglbtnC4);
+				
+				JToggleButton tglbtnD1 = new JToggleButton("D1");
+				tglbtnD1.setFont(new Font("Tahoma", Font.PLAIN, 8));
+				tglbtnD1.setBounds(131, 248, 45, 45);
+				panelAssentos.add(tglbtnD1);
+				
+				JToggleButton tglbtnD2 = new JToggleButton("D2");
+				tglbtnD2.setFont(new Font("Tahoma", Font.PLAIN, 8));
+				tglbtnD2.setBounds(186, 248, 45, 45);
+				panelAssentos.add(tglbtnD2);
+				
+				JToggleButton tglbtnD3 = new JToggleButton("D3");
+				tglbtnD3.setFont(new Font("Tahoma", Font.PLAIN, 8));
+				tglbtnD3.setBounds(241, 248, 45, 45);
+				panelAssentos.add(tglbtnD3);
+				
+				JToggleButton tglbtnD4 = new JToggleButton("D4");
+				tglbtnD4.setFont(new Font("Tahoma", Font.PLAIN, 8));
+				tglbtnD4.setBounds(296, 248, 45, 45);
+				panelAssentos.add(tglbtnD4);
+				
+				}
+				
+			
+			}
+		});
 		
-		JToggleButton tglbtnA4 = new JToggleButton("A4");
-		tglbtnA4.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		tglbtnA4.setBounds(296, 80, 45, 45);
-		panelAssentos.add(tglbtnA4);
+
 		
-		JToggleButton tglbtnB1 = new JToggleButton("B1");
-		tglbtnB1.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		tglbtnB1.setBounds(131, 136, 45, 45);
-		panelAssentos.add(tglbtnB1);
 		
-		JToggleButton tglbtnB2 = new JToggleButton("B2");
-		tglbtnB2.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		tglbtnB2.setBounds(186, 136, 45, 45);
-		panelAssentos.add(tglbtnB2);
-		
-		JToggleButton tglbtnB3 = new JToggleButton("B3");
-		tglbtnB3.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		tglbtnB3.setBounds(241, 136, 45, 45);
-		panelAssentos.add(tglbtnB3);
-		
-		JToggleButton tglbtnB4 = new JToggleButton("B4");
-		tglbtnB4.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		tglbtnB4.setBounds(296, 136, 45, 45);
-		panelAssentos.add(tglbtnB4);
-		
-		JToggleButton tglbtnC1 = new JToggleButton("C1");
-		tglbtnC1.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		tglbtnC1.setBounds(131, 192, 45, 45);
-		panelAssentos.add(tglbtnC1);
-		
-		JToggleButton tglbtnC2 = new JToggleButton("C2");
-		tglbtnC2.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		tglbtnC2.setBounds(186, 192, 45, 45);
-		panelAssentos.add(tglbtnC2);
-		
-		JToggleButton tglbtnC3 = new JToggleButton("C3");
-		tglbtnC3.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		tglbtnC3.setBounds(241, 192, 45, 45);
-		panelAssentos.add(tglbtnC3);
-		
-		JToggleButton tglbtnC4 = new JToggleButton("C4");
-		tglbtnC4.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		tglbtnC4.setBounds(296, 192, 45, 45);
-		panelAssentos.add(tglbtnC4);
-		
-		JToggleButton tglbtnD1 = new JToggleButton("D1");
-		tglbtnD1.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		tglbtnD1.setBounds(131, 248, 45, 45);
-		panelAssentos.add(tglbtnD1);
-		
-		JToggleButton tglbtnD2 = new JToggleButton("D2");
-		tglbtnD2.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		tglbtnD2.setBounds(186, 248, 45, 45);
-		panelAssentos.add(tglbtnD2);
-		
-		JToggleButton tglbtnD3 = new JToggleButton("D3");
-		tglbtnD3.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		tglbtnD3.setBounds(241, 248, 45, 45);
-		panelAssentos.add(tglbtnD3);
-		
-		JToggleButton tglbtnD4 = new JToggleButton("D4");
-		tglbtnD4.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		tglbtnD4.setBounds(296, 248, 45, 45);
-		panelAssentos.add(tglbtnD4);
 				
 		
 		JPanel panelPagamento = new JPanel();
@@ -482,5 +505,22 @@ public class Main {
 		
 		return filmeSelecionado;
 		
+	}
+	
+	public Assento retornaAssento(List<Assento> listaDeAssentos, Filme filmeSelecionado, int indice) {
+		
+		Assento assento = null;
+		
+		if (filmeSelecionado != null) {
+			
+			assento = listaDeAssentos.get(indice);
+			
+		}
+		
+		return assento;
+		
+	}
+	public JPanel getPanelEntrar() {
+		return panelEntrar;
 	}
 }
